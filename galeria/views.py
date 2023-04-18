47f76fd0bfaf36b9efe8320b1ea120ece1b2821a
André Segatto <segatto9@gmail.com>
2023-04-18 08:39:16 -0300

    Dicionario de dados para testar lista de repeticao Django

--- galeria/views.py
+++ galeria/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 
-dados = {
+
+
+def index(request):
+    dados = {
     1: {"nome": "Nebulosa de Carina",
         "legenda": "webbtelescope.org / NASA / James Webb"},
     2: {"nome": "Galáxia NGC 1079",
         "legenda": "nasa.org / NASA / Hubble"}
-}
-
-def index(request):
-    return render(request, 'galeria/index.html')
+    }
+    
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
