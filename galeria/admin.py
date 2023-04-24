eb7fdbe135a15135f1ac3677a36a3ce34a95f6d5
André Segatto <segatto9@gmail.com>
2023-04-24 08:38:10 -0300

    Boolean 'Publicada' em models

--- galeria/admin.py
+++ galeria/admin.py
@@ -5,6 +5,8 @@ class ListandoFotografias(admin.ModelAdmin):
     list_display = ('id', 'nome', 'categoria', 'legenda')
     list_display_links = ('id', 'nome', 'legenda')
     search_fields = ('nome', 'legenda', 'categoria')
+    list_filter = ('categoria',)
+    list_per_page = 10
 
 admin.site.register(Fotografia, ListandoFotografias)
 
--- galeria/models.py
+++ galeria/models.py
@@ -12,11 +12,13 @@ class Fotografia(models.Model):
         ('GALÁXIA', 'Galáxia'),
         ('PLANETA', 'Planeta')
     ]
+    
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=True, blank=True)
     categoria = models.CharField(max_length=100, default='', choices=OPCOES_CATEGORIA)
     descricao = models.TextField(null=True, blank=True)
     foto = models.CharField(max_length=100, null=False, blank=False)
+    publicada = models.BooleanField(default=False)
     
     def __str__(self):
         return f"Fotografia [nome={self.nome}]"
