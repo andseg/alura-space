f6393db393cf7c9f19825567e81b830f32170ea8
André Segatto <segatto9@gmail.com>
2023-04-24 11:49:53 -0300

    Modulo Pillow instalado, Upload de fotos, Foto padrao 404 adicionada

--- galeria/admin.py
+++ galeria/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from galeria.models import Fotografia
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'categoria', 'legenda')
+    list_display = ('id', 'nome', 'categoria', 'legenda', 'publicada', 'data_fotografia')
     list_display_links = ('id', 'nome', 'legenda')
     search_fields = ('nome', 'legenda', 'categoria')
     list_filter = ('categoria',)
+    list_editable = ('publicada', 'data_fotografia')
     list_per_page = 10
 
 admin.site.register(Fotografia, ListandoFotografias)
--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
@@ -17,8 +18,9 @@ class Fotografia(models.Model):
     legenda = models.CharField(max_length=150, null=True, blank=True)
     categoria = models.CharField(max_length=100, default='', choices=OPCOES_CATEGORIA)
     descricao = models.TextField(null=True, blank=True)
-    foto = models.CharField(max_length=100, null=False, blank=False)
+    foto = models.ImageField(upload_to='fotos/%Y/%m/%d/', blank=True)
     publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
     
     def __str__(self):
         return f"Fotografia [nome={self.nome}]"
